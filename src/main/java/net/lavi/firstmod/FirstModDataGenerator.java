5f821ed58e311424946c1a4ae10c143e9081aa2d
lavimanor <lavimanor12@gmail.com>
2026-04-26 19:19:01 +0300

    DataGen

--- src/main/java/net/lavi/firstmod/FirstModDataGenerator.java
+++ src/main/java/net/lavi/firstmod/FirstModDataGenerator.java
@@ -2,10 +2,17 @@ package net.lavi.firstmod;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.lavi.firstmod.datagen.*;
 
 public class FirstModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
---
+++ src/main/java/net/lavi/firstmod/datagen/ModItemTagProvider.java
@@ -0,0 +1,26 @@
+package net.lavi.firstmod.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.lavi.firstmod.item.ModItems;
+import net.lavi.firstmod.util.ModTags;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
+    public ModItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
+        super(output, completableFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup lookup) {
+        getOrCreateTagBuilder(ModTags.Items.TRANSFORMABLE_ITEMS)
+                .add(ModItems.PINK_GARNET)
+                .add(ModItems.RAW_PINK_GARNET)
+                .add(Items.COAL)
+                .add(Items.STICK)
+                .add(Items.APPLE);
+    }
+}
